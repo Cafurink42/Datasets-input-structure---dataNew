e1971669d57e08f5a88bb8024bca9410f60542c3
Carurink42 <carlosdanielcotticaguth@gmail.com>
2026-09-21 20:59:00 -0300

    First commit

---
+++ dataNew.py
@@ -0,0 +1,22 @@
+import json
+
+lista = []
+while True:
+    data01 = input("Input do usuário:")
+    data02 = input("Output do modelo:")
+    if data02 == 'sair':
+        break
+    dataset = (f'{{"instruction": "{data01}", "input": "", "output": "{data02}"}}')
+    lista.append(dataset)
+
+    #dataset02 = (f'{{"instruction": "{data01}", "input": "", "output": "{data02}"}}')
+
+
+json_string = json.dumps(lista)
+
+json_string = [json.loads(i) for i in lista]
+
+with open("DATASETS/output.json", "w") as file:
+    json.dump(json_string, file, indent=4)
+    
+#{"instruction": "", "input":"", "output": ""}
